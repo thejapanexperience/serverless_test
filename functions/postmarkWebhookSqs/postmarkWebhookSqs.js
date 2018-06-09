34a2e0ed36ec96fc33550b06b446ee3bb355090a
Richard Mands <richard.mands@sage.com>
2018-06-09 16:09:14 +0100

    wip refactored lambda 1 to make testing easier aot

--- functions/postmarkWebhookSqs/postmarkWebhookSqs.js
+++ functions/postmarkWebhookSqs/postmarkWebhookSqs.js
@@ -1,72 +1,60 @@
 const AWS = require('aws-sdk')
 
-exports.handler = async (event) => {
+let sqs = new AWS.SQS(options = {})
+let sns = new AWS.SNS(options = {})
 
-  let sqs = new AWS.SQS(options = {})
-  let sns = new AWS.SNS(options = {})
-
-  let statusCode = 204
-
-  let dummyEvent = {
-    body: JSON.stringify({
-      name: "Richard",
-      status: "Awesome"
-    })
-  }
-
-  event ? event : event = dummyEvent
-
-  let sqsParams = {
-    MessageBody: event.body,
-    QueueUrl: process.env.SQS_URL,
-    MessageAttributes: {
-      'WebhookData': {
-        DataType: 'String',
-        StringValue: 'My Data'
-      },
+const sqsParams = {
+  MessageBody: '',
+  QueueUrl: process.env.SQS_URL,
+  MessageAttributes: {
+    'WebhookData': {
+      DataType: 'String',
+      StringValue: 'My Data'
     },
-  };
- 
-  let snsParams = {
-    Message: 'I am a trigger',
-    TopicArn: process.env.SNS_ARN
-  };
-  
-  const putWebhookUpdateOnSqs = () => new Promise((resolve, reject) => {
-    sqs.sendMessage( sqsParams,(err, data) => {
-      if (err) {
-        console.log('Error: ', err, err.stack); // an error occurred
-        reject(err)
-      }
-      else {
-        // console.log('Response: ', data); // successful response
-        resolve(data)
-      } 
-    })
-  })
-  
-  const snsTrigger = () => new Promise((resolve, reject) => {
-    sns.publish( snsParams,(err, data) => {
-      if (err) {
-        console.log('Error: ', err, err.stack); // an error occurred
-        reject(err)
-      }
-      else {
-        console.log('Response: ', data); // successful response
-        resolve(data)
-      } 
-    })
-  })
+  },
+};
 
+const putWebhookUpdateOnSqs = (event) => new Promise((resolve, reject) => {
+  let params = {...sqsParams}
+  params.MessageBody = event.body
+  sqs.sendMessage(params, (err, data) => {
+    if (err) {
+      console.log('Error: ', err, err.stack); // an error occurred
+      reject(err)
+    } else {
+      console.log('Response: ', data); // successful response
+      resolve(data)
+    }
+  })
+})
+
+const snsParams = {
+  Message: 'I am a trigger',
+  TopicArn: process.env.SNS_ARN
+};
+
+const snsTrigger = () => new Promise((resolve, reject) => {
+  sns.publish(snsParams, (err, data) => {
+    if (err) {
+      console.log('Error: ', err, err.stack); // an error occurred
+      reject(err)
+    } else {
+      console.log('Response: ', data); // successful response
+      resolve(data)
+    }
+  })
+})
+
+exports.handler = async (event, context) => {
+  
+  if (event.headers.giraffe !== 'hippo'){
+    return { statusCode: 403 }
+  }
+  
   console.log('Putting webhook data on queue...')
-  await putWebhookUpdateOnSqs()
+  await putWebhookUpdateOnSqs(event)
   console.log('Triggering lambda via SNS...')
   await snsTrigger()
   console.log('Done!')
-  
-  let response = {
-    statusCode: statusCode
-  }
-
-  return response
+  return { statusCode: 204 }
 };
